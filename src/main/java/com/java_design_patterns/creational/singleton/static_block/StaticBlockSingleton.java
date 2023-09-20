f565061f450c0ef2681229e2984fe483075baeec
Farzan Saketi <farzan.6118@gmail.com>
2023-09-20 11:28:57 +0330

    jdk version upgraded to 17 a typo corrected

--- src/main/java/com/java_design_patterns/creational/singleton/static_block/StaticBlockSingleton.java
+++ src/main/java/com/java_design_patterns/creational/singleton/static_block/StaticBlockSingleton.java
@@ -2,14 +2,14 @@ package com.java_design_patterns.creational.singleton.static_block;
 
 public class StaticBlockSingleton {
 
-    private static StaticBlockSingleton instance;
+    private static final StaticBlockSingleton instance;
 
     //static block initialization for exception handling
     static {
         try {
             instance = new StaticBlockSingleton();
         } catch (Exception e) {
-            throw new RuntimeException("Exception occured in creating singleton instance");
+            throw new RuntimeException("Exception occurred in creating singleton instance");
         }
     }
 
